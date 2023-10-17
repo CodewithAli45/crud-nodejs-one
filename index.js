5253079d9ddd4b6cc22997f9e1f97fe299c1764f
Ali Murtaza <112254545+CodewithAli45@users.noreply.github.com>
2023-10-17 11:35:07 +0530

    cors added

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ app.use(express.json());
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT;
-
+const cors = require('cors');
 const router = require('./routes/userRoutes');
 
-
+app.use(cors());
 const connectToDatabase = require('./db/database');
 app.use(connectToDatabase);
 app.use('/api/v1', router)
